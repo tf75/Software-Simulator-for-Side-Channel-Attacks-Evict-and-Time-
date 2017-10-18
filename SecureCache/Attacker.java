fcaa17637cde5629c9b0a55d05ba3a6e1493e0dd
tf75 <kingsleytfwork@gmail.com>
2017-10-18 11:13:47 +0100

    Add files via upload

---
+++ SecureCache/Attacker.java
@@ -0,0 +1,204 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+class Attacker {
+
+  /*Initalises the cache */
+  SecureCache sc = new SecureCache();
+
+
+  /*Store all plaintext bytes */
+  ArrayList<Integer> plainarray = new ArrayList<Integer>();
+  /*Stores key candidates for 1 plaintextbyte */
+  int[] setsforevict = new int[256];
+  /*To record the start and end of plaintextbyte array */
+  int start = 0;
+  int end = 16;
+
+  /*Store results from attack */
+  ArrayList<Integer> list = new ArrayList<Integer>();
+
+
+
+  /*This is from the input and displays which plaintext bytes are being considered for the attack */
+  int inputnumber = 0;
+
+  /*Address stream from file */
+  ArrayList<String> addressstream = new ArrayList<String>();
+
+/*To begin the attack and initalise the cache */
+ void begin(ArrayList<Integer> plaintext, int numSet, int numBlock, int input, int setasoc){
+     plainarray.addAll(plaintext);
+     numset = numSet;
+     numblock = numBlock;
+     sc.initalisecache(numSet, numBlock, setasoc);
+     inputnumber = input;
+     readfile();
+  }
+
+/*From the outputted file after the filetrimmer getting the address stream */
+  void readfile(){
+    ArrayList<String> listoffiles = new ArrayList<String>();
+    File dir = new File("/home/tf75/Documents/Master's project/Securecache/hexmemtraces04092017");
+    File[] files = dir.listFiles();
+    Arrays.sort(files); /*list.files() does not save files in particular order so this function sorts them numerically */
+    for (File filename : files) {
+      if(filename.getName().startsWith("hex")){
+      try{
+        FileReader inputFile = new FileReader(filename);
+        BufferedReader bufferReader = new BufferedReader(new FileReader(filename));
+        String line;
+        while ((line = bufferReader.readLine()) != null){
+                addressstream.add(line);
+        }
+           bufferReader.close(); // Closes the input stream
+      }
+       catch(IOException e){
+             e.printStackTrace(); //It prints a stack trace for this Throwable object on the error output stream that is the value of the field
+             System.exit(0);
+       }
+         System.out.println("Now it will begin to run the simulation for " + filename);
+         String a = "Time taken to run attack for one encryption beg " + new SimpleDateFormat("HH.mm.ss.SS.").format(new Date());
+         System.out.println(a);
+         beginsimulation();
+         String b = "Time taken to run attack for one encryption end " + new SimpleDateFormat("HH.mm.ss.SS.").format(new Date());
+         System.out.println(b);
+         System.out.println("I am about to clear addressstream");
+
+    }
+    addressstream.clear();
+   }
+  }
+
+  void beginsimulation(){
+       int loopnum = 0;
+       while(start<end){
+           if(inputnumber > loopnum){
+           String c = "Time taken for one address read beg " + new SimpleDateFormat("HH.mm.ss.SS.").format(new Date());
+           System.out.println(c);
+           timewithoutevict();
+       	   caculateevictsets(start);
+           loopnum++; /*Loop number is how file output is incremeneted as well so incremented before output */
+           runattack(loopnum);
+           String d = "Time taken for one address read end " + new SimpleDateFormat("HH.mm.ss.SS.").format(new Date());
+           System.out.println(d);
+           }
+           start++;
+      }
+     /*This ensures that it will count the next number of plaintext bytes, all should be a sequence of 16 */
+     end += 16;
+  }
+
+  void timewithoutevict(){
+  	    runaddressstream();
+  	    runaddressstreamsecond();
+        list.add(sc.return_hit());
+        list.add(sc.return_miss());
+        list.add(0);
+        list.add(0);
+        list.add(0);
+        flushthecache();
+  }
+
+/*This gathers candidate keys */
+  void caculateevictsets(int plainnum){
+       int temp;
+       Long l;
+       for(int i = 0; i<256; i++){
+           temp = plainarray.get(plainnum) ^ i;
+           l = Long.valueOf(temp);
+           setsforevict[i] = return_set(l);
+       }
+  }
+
+  int return_set(Long address){
+      address += 0x6041a0; /*This is t-table look up for hexadecimal addresses */
+      int intresult = sc.rmtrand(address);
+      return intresult;
+   }
+
+  void evictset(int set){
+  	   sc.evictset(set);
+  }
+
+/*This tells the cache to run the address stream */
+  void runaddressstream(){
+    /*Secure cache runs this address stream to caculate protected sets */
+  	   sc.execute_encryption_firstround(addressstream);
+  }
+
+  void runaddressstreamsecond(){
+      /*This is a normal address run without calculation of protected sets */
+       sc.execute_encryption_secondround(addressstream);
+  }
+
+/*This removes all data from the cache */
+  void flushthecache(){
+  	   sc.flushcache();
+  }
+
+/*This gathers the hit and misses from the file */
+  void runattack(int loopnum){
+    for(int x = 0; x<256; x++){
+        runaddressstreamsecond();
+        evictset(setsforevict[x]);
+        runaddressstreamsecond();
+        list.add(sc.return_hit());
+        list.add(sc.return_miss());
+        list.add(x);
+        list.add(plainarray.get(start));
+        list.add(setsforevict[x]);
+        flushthecache();
+    }
+    outputfile(loopnum);
+  }
+
+
+ void outputfile(int num){
+       /*You also need to add the plaintext value into results */
+       if(num > 16){
+         System.err.println("The loopnum has gone above 16");
+         System.exit(0);
+       }
+       int line = 0;
+       String fileDir = "/home/tf75/Documents/Master's project/Securecache/output";
+       String fileName = "result" + new SimpleDateFormat("yyyy.MM.dd.HHmmss.SS!").format(new Date()) + num;
+       File f = new File (fileDir,fileName);
+       try{
+           PrintWriter writer = new PrintWriter(f);
+             for(int value: list){
+                 line++;
+                 String printed = Integer.toString(value);
+                 /*H = HITS, M = MISS, K = KEY, P = PLAINTEXT, S = SET */
+                 if(line == 1){
+                    printed = "H " + printed;
+                 }
+                 else if(line == 2){
+                 printed = "M " + printed;
+                 }
+                 else if(line == 3){
+                  printed = "K " + printed;
+                 }
+                 else if(line == 4){
+                  printed = "P " + printed;
+                 }
+             else{
+                 printed = "S " + printed;
+                 line = 0;
+             }
+             writer.write(printed);
+             writer.println();
+            }
+             writer.close();
+       }catch(IOException e){
+         e.printStackTrace();
+          System.exit(0);
+         }
+      list.clear();
+    }
+}
---
+++ SecureCache/Input.java
@@ -0,0 +1,155 @@
+import java.util.*;
+import java.io.*;
+
+class Input{
+
+/*To keep track of line number in case an error is thrown */
+  int linenum;
+  /*Store all plaintext bytes for Attacker */
+  ArrayList<Integer> plaintext = new ArrayList<Integer>();
+
+	public static void main(String[] args){
+	 Input i = new Input();
+	 i.setinputs();
+	}
+
+/*First it is input of sets or if set assoc is 1, total num of blocks */
+	void setinputs(){
+     System.out.println("Please input the power for amount of amount sets");
+     Scanner in = new Scanner(System.in);
+      int num = in.nextInt();
+        if(num >=  1){
+          int numset = num;
+           setinputs_numbytes(numset);
+        }
+        else{
+          /*If incorrect does not exit but just repeats till successful input */
+        	System.out.println("Please input a power of equal to or greater than 1");
+        	setinputs();
+        }
+	}
+
+/*Input power of the size of per block */
+	void setinputs_numbytes(int numset){
+    int numbytes;
+		System.out.println("Please input the power of memory space in the cache blocks per set (2 to the power of 2 will be 4 bytes)");
+		Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        if(num >=  1){
+           numbytes = num;
+           set_associative(numset, numbytes);
+        }
+        else{
+        	System.out.println("Please input a power of equal to or greater than 1");
+        	setinputs_numbytes(numset);
+        }
+	}
+
+/*To set the set-assoc level, this is not a power of but just integer */
+  void set_associative(int numset, int numbytes){
+    int setasoc;
+    System.out.println("Please input the number of set associative");
+    Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        if(num >=  1){
+           setasoc = num;
+           setinputs_plaintextbytes(numset, numbytes, setasoc);
+        }
+        else{
+          System.out.println("Please input a number equal to or greater than 1");
+          set_associative(numset, numbytes);
+        }
+  }
+
+	void setinputs_plaintextbytes(int numset, int numbyte, int setasoc){
+		 int i = 0;
+		 int total = 0;
+         System.out.println("This will now read all the plaintext bytes in the file plaintext.txt");
+         try {
+         	    String fileDir = "/home/index/plaintext";
+              String fileName = "plaintext.txt"; /*This copies all the plaintext bytes into one array list */
+              File f = new File (fileDir,fileName);
+              Scanner scanner = new Scanner(f);
+              while(scanner.hasNext()){
+              plaintext.add(scanner.nextInt(16));
+              /*Plaintext bytes should be equal to or less than 255 decimal value */
+               if(plaintext.get(total) > 255){
+               System.out.println("This plaintext byte is too large, please ensure they lie in the range of 0x00 to 0xff");
+               System.exit(0);
+               }
+              total++;
+              }
+          }
+		  catch(IOException e){
+             e.printStackTrace(); //It prints a stack trace for this Throwable object on the error output stream that is the value of the field
+             System.exit(0);
+          }
+    /*Each line should of had 16 bytes */
+		 if(total % 16 == 0){
+		 }
+		 else{
+		 	System.out.println("Please make sure that byte values are a multiple of 16 " + total);
+            System.exit(0);
+		 }
+		 System.out.println("The list of plaintext bytes has been parsed. Total plaintext bytes are " + total);
+         settingupattack(numset, numbyte, setasoc);
+	}
+
+	 void settingupattack(int numset, int numbyte, int setasoc){
+
+     checkfiles(); /*This checks that all files in the hexmemtraces are of hexadecimal addresses */
+
+     /*Adds how many plaintext bytes read in, if you put 1 it will read from position 0 and so on */
+     System.out.println("How many plaintext bytes do you want to read? 1-16");
+     Scanner in = new Scanner(System.in);
+     int num = in.nextInt();
+     if(num > 0 && num <= 16){
+     /*This initalises the attacker java class */
+     Attacker attacker = new Attacker();
+     /*This simply adds the plaintext byte array to the attacker */
+      attacker.begin(plaintext, numset, numbyte, num, setasoc);
+    }
+    else{
+      System.out.println("Please put in number between 1-16");
+      settingupattack(numset, numbyte, setasoc);
+    }
+
+	}
+
+  /*This checks that all files within the memory trace folder are of hexadecimal addresses */
+  void checkfiles(){
+  	System.out.println("The hex files will now be parsed");
+    File directory = new File("/home/index");
+    File files[] = directory.listFiles();
+    for (File filename : files) {
+      try{
+          FileReader inputFile = new FileReader(filename);
+          BufferedReader bufferReader = new BufferedReader(new FileReader(filename));
+          String line;
+          int linecount = 0;
+           while ((line = bufferReader.readLine()) != null){
+            linecount++;
+            /*Checks whether the address value read in is hexadecimal notation */
+            check_hex(line, linecount, filename);
+           }
+           bufferReader.close(); // Closes the input stream
+           }
+       catch(IOException e){
+             e.printStackTrace(); //It prints a stack trace for this Throwable object on the error output stream that is the value of the field
+             System.exit(0);
+       }
+     }
+     System.out.println("Hex files successfully parsed");
+  }
+
+  /*This tests whether all the file is of hex inputs */
+   void check_hex(String hex, int linecount, File filename){
+     boolean isNumeric = hex.matches("\\p{XDigit}+");
+     if(isNumeric == false){
+        assert(isNumeric == false);
+        /*Will print out the line and where it stopped parsing so the user can correct this */
+        System.out.println("not a valid hex input " + hex + " on line " + linecount + "of file " + filename);
+        System.exit(0);
+     }
+   }
+}

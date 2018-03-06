c602d2e53a92ff59b46c2438377f779688aec13d
tf75 <kingsleytfwork@gmail.com>
2018-03-06 22:55:11 +0000

    Add files via upload

--- SetCache/Attacker.java
+++ SetCache/Attacker.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 class Attacker {
 
   /*Initalises the cache */
-  SecureCache sc = new SecureCache();
+  SetCache sc = new SetCache();
 
 
   /*Store all plaintext bytes */
@@ -19,10 +19,8 @@ class Attacker {
   /*To record the start and end of plaintextbyte array */
   int start = 0;
   int end = 16;
-
-  /*Store results from attack */
+  /*This is to list the results from attack */
   ArrayList<Integer> list = new ArrayList<Integer>();
-
 
 
   /*This is from the input and displays which plaintext bytes are being considered for the attack */
@@ -34,8 +32,7 @@ class Attacker {
 /*To begin the attack and initalise the cache */
  void begin(ArrayList<Integer> plaintext, int numSet, int numBlock, int input, int setasoc){
      plainarray.addAll(plaintext);
-     numset = numSet;
-     numblock = numBlock;
+     /*To initalise cache with size dimensions */
      sc.initalisecache(numSet, numBlock, setasoc);
      inputnumber = input;
      readfile();
@@ -44,49 +41,50 @@ class Attacker {
 /*From the outputted file after the filetrimmer getting the address stream */
   void readfile(){
     ArrayList<String> listoffiles = new ArrayList<String>();
-    File dir = new File("/home/tf75/Documents/Master's project/Securecache/hexmemtraces04092017");
+    File dir = new File("/home/index");
     File[] files = dir.listFiles();
     Arrays.sort(files); /*list.files() does not save files in particular order so this function sorts them numerically */
     for (File filename : files) {
+      /*Output from FileTrimmer were hex files */
       if(filename.getName().startsWith("hex")){
-      try{
-        FileReader inputFile = new FileReader(filename);
-        BufferedReader bufferReader = new BufferedReader(new FileReader(filename));
-        String line;
-        while ((line = bufferReader.readLine()) != null){
-                addressstream.add(line);
-        }
-           bufferReader.close(); // Closes the input stream
+         try{
+             FileReader inputFile = new FileReader(filename);
+             BufferedReader bufferReader = new BufferedReader(new FileReader(filename));
+             String line;
+             while ((line = bufferReader.readLine()) != null){
+                     addressstream.add(line);
+             }
+        bufferReader.close(); // Closes the input stream
       }
-       catch(IOException e){
-             e.printStackTrace(); //It prints a stack trace for this Throwable object on the error output stream that is the value of the field
-             System.exit(0);
+      catch(IOException e){
+                          e.printStackTrace(); //It prints a stack trace for this Throwable object on the error output stream that is the value of the field
+                          System.exit(0);
        }
-         System.out.println("Now it will begin to run the simulation for " + filename);
-         String a = "Time taken to run attack for one encryption beg " + new SimpleDateFormat("HH.mm.ss.SS.").format(new Date());
-         System.out.println(a);
-         beginsimulation();
-         String b = "Time taken to run attack for one encryption end " + new SimpleDateFormat("HH.mm.ss.SS.").format(new Date());
-         System.out.println(b);
-         System.out.println("I am about to clear addressstream");
-
+       /*Time outputs were added in to time output and check the program isn't stuck in a loop */
+      System.out.println("Now it will begin to run the simulation for " + filename);
+      String a = "Time taken to run attack for one encryption beg " + new SimpleDateFormat("HH.mm.ss.SS.").format(new Date());
+      System.out.println(a);
+      beginsimulation();
+      String b = "Time taken to run attack for one encryption end " + new SimpleDateFormat("HH.mm.ss.SS.").format(new Date());
+      System.out.println(b);
+      System.out.println("I am about to clear addressstream");
     }
-    addressstream.clear();
+   addressstream.clear();
    }
-  }
+ }
 
   void beginsimulation(){
        int loopnum = 0;
        while(start<end){
+           /*The loop will only output files if specified by the user as input number */
            if(inputnumber > loopnum){
-           String c = "Time taken for one address read beg " + new SimpleDateFormat("HH.mm.ss.SS.").format(new Date());
-           System.out.println(c);
-           timewithoutevict();
+           timewithoutevict(); /*This is for if the user wants to know a standard time without evict */
        	   caculateevictsets(start);
-           loopnum++; /*Loop number is how file output is incremeneted as well so incremented before output */
+           loopnum++; /*Loop number is how Attacker output is incremeneted as well so incremented before output */
            runattack(loopnum);
-           String d = "Time taken for one address read end " + new SimpleDateFormat("HH.mm.ss.SS.").format(new Date());
-           System.out.println(d);
+           }
+           else{
+            loopnum++;
            }
            start++;
       }
@@ -95,46 +93,42 @@ class Attacker {
   }
 
   void timewithoutevict(){
-  	    runaddressstream();
-  	    runaddressstreamsecond();
-        list.add(sc.return_hit());
-        list.add(sc.return_miss());
-        list.add(0);
-        list.add(0);
-        list.add(0);
-        flushthecache();
+  	runaddressstream();
+  	runaddressstream();
+  	list.add(sc.return_hit());
+    list.add(sc.return_miss());
+    /*To add in Key, Plaintext Byte and Set */
+    list.add(0);
+    list.add(0);
+    list.add(0);
+    flushthecache();
   }
 
 /*This gathers candidate keys */
   void caculateevictsets(int plainnum){
-       int temp;
+       Long temp;
        Long l;
        for(int i = 0; i<256; i++){
-           temp = plainarray.get(plainnum) ^ i;
-           l = Long.valueOf(temp);
-           setsforevict[i] = return_set(l);
+           temp = Long.valueOf(plainarray.get(plainnum) ^ i);
+           setsforevict[i] = return_set(temp);
        }
   }
 
   int return_set(Long address){
       address += 0x6041a0; /*This is t-table look up for hexadecimal addresses */
-      int intresult = sc.rmtrand(address);
-      return intresult;
+      int intresult = sc.return_set(address);
+      return intresult; /*To get set of cand key XOR plaintext + T-table address */
    }
 
+/*Simply evicts the set from cache */
   void evictset(int set){
   	   sc.evictset(set);
   }
 
+
 /*This tells the cache to run the address stream */
   void runaddressstream(){
-    /*Secure cache runs this address stream to caculate protected sets */
-  	   sc.execute_encryption_firstround(addressstream);
-  }
-
-  void runaddressstreamsecond(){
-      /*This is a normal address run without calculation of protected sets */
-       sc.execute_encryption_secondround(addressstream);
+  	   sc.execute_encryption(addressstream);
   }
 
 /*This removes all data from the cache */
@@ -142,63 +136,65 @@ class Attacker {
   	   sc.flushcache();
   }
 
+
 /*This gathers the hit and misses from the file */
   void runattack(int loopnum){
-    for(int x = 0; x<256; x++){
-        runaddressstreamsecond();
-        evictset(setsforevict[x]);
-        runaddressstreamsecond();
-        list.add(sc.return_hit());
-        list.add(sc.return_miss());
-        list.add(x);
-        list.add(plainarray.get(start));
-        list.add(setsforevict[x]);
-        flushthecache();
-    }
-    outputfile(loopnum);
+    /* 256 candidate keys from value 0-255 */
+  	for(int x = 0; x<256; x++){
+  		 runaddressstream();
+  		 evictset(setsforevict[x]);
+  		 runaddressstream();
+       list.add(sc.return_hit());
+       list.add(sc.return_miss());
+       list.add(x);
+       list.add(plainarray.get(start));
+       list.add(setsforevict[x]);
+       flushthecache();
+  	}
+  	outputfile(loopnum);
   }
 
-
+/*Outputs the Attacker's data from the attack */
  void outputfile(int num){
-       /*You also need to add the plaintext value into results */
        if(num > 16){
          System.err.println("The loopnum has gone above 16");
          System.exit(0);
        }
        int line = 0;
-       String fileDir = "/home/tf75/Documents/Master's project/Securecache/output";
+       String fileDir = "/home/";
        String fileName = "result" + new SimpleDateFormat("yyyy.MM.dd.HHmmss.SS!").format(new Date()) + num;
        File f = new File (fileDir,fileName);
        try{
            PrintWriter writer = new PrintWriter(f);
-             for(int value: list){
+           for(int value: list){
                  line++;
                  String printed = Integer.toString(value);
-                 /*H = HITS, M = MISS, K = KEY, P = PLAINTEXT, S = SET */
+                 /* H = HIT, M = MISS, K = KEY, P = PLAINTEXT, S = SET */
                  if(line == 1){
                     printed = "H " + printed;
                  }
                  else if(line == 2){
-                 printed = "M " + printed;
+                         printed = "M " + printed;
                  }
                  else if(line == 3){
-                  printed = "K " + printed;
+                         printed = "K " + printed;
                  }
                  else if(line == 4){
-                  printed = "P " + printed;
+                         printed = "P " + printed;
                  }
-             else{
-                 printed = "S " + printed;
-                 line = 0;
-             }
-             writer.write(printed);
-             writer.println();
-            }
-             writer.close();
-       }catch(IOException e){
-         e.printStackTrace();
-          System.exit(0);
-         }
+                 else{
+                      printed = "S " + printed;
+                      line = 0;
+                 }
+          writer.write(printed);
+          writer.println();
+          }
+       writer.close();
+       }
+       catch(IOException e){
+            e.printStackTrace();
+            System.exit(0);
+       }
       list.clear();
-    }
+  }
 }
---
+++ SetCache/SetCache.java
@@ -0,0 +1,195 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math.*;
+
+class SetCache{
+
+  /* numset represents the number of sets whilst numblock is the amount of blocks per set */
+  int numset;
+  int numblock;
+
+  /*Sum of the number of sets and cache blocks per set */
+  int sumset;
+
+  /*set associativity number */
+  int setassoc;
+
+  /*to record the hit and misses the simulation accumulate */
+  int hit;
+  int necessarymiss;
+  int capacitymiss;
+
+  /*The arraylist representing the memory cache */
+  ArrayList<ArrayList<Integer>> memcache = new ArrayList<ArrayList<Integer>>();
+
+  /*To build the cache based on inputs from the user of the power of based on user input */
+  void initalisecache(int setnum, int numblocks, int setasoc){
+   /*To initalise the attacker, the 'power of' inputs are used in getting set and tag from the address */
+   numset = setnum;
+   numblock = numblocks;
+   setassoc = setasoc;
+   sumset = (int) Math.pow(2, setnum);
+   int sumblock = (int) Math.pow(2, numblocks);
+   int memsize = (sumset*sumblock) * setassoc;
+   System.out.println("This is the total number of bytes in the cache");
+   System.out.println(memsize);
+   for(int i = 0; i <sumset; i++){
+    memcache.add(new ArrayList<Integer>());
+   }
+    for(int i = 0; i<sumset; i++){
+     for(int x = 0; x<setassoc; x++){
+            memcache.get(i).add(x, null);
+     }
+    }
+  }
+
+
+/*This reads in the address stream from the attacker */
+  void execute_encryption(ArrayList<String> addressstream){
+  	  hit = 0;
+      necessarymiss = 0;
+      capacitymiss = 0;
+      int tag = 0;
+      Long num = 0l;
+      int set = 0;
+      for(int x = 0; x < addressstream.size(); x++){
+         num = Long.parseLong(addressstream.get(x), 16);
+         set = return_set(num);
+         if(set > sumset){
+           System.out.println("The set inputted is greater than the sets available");
+           System.exit(0);
+         }
+      assert(set <= sumset);
+      tag = return_tag(num);
+      hitandmiss(tag, set);
+     }
+    /*This is to ensure that hits and misses are equal to the address size for testing */
+    if((hit + necessarymiss + capacitymiss) != addressstream.size()){
+       System.out.println("There is too few or many hits and misses for this address stream");
+       System.exit(0);
+    }
+    assert((hit + necessarymiss + capacitymiss) == addressstream.size());
+  }
+
+  /*To return the amount of hits per addresss stream read */
+  int return_hit(){
+   return hit;
+  }
+
+  /*To return the amount of misses per address stream read */
+  int return_miss(){
+    int miss = necessarymiss + capacitymiss;
+  	return miss;
+  }
+
+  void evictset(int set){
+  	   if(set > sumset){
+  	   	System.err.println("set being evicted is greater than the sumset");
+        System.exit(0);
+  	   }
+       assert(set <= sumset);
+       /*This deletes all blocks in the set */
+       for(int i = 0; i<setassoc; i++){
+       	   memcache.get(set).set(i, null);
+       }
+  }
+
+   void hitandmiss(int tag, int set){
+      /*This checks whether there is a space not filled yet or if the tag is present */
+      Boolean capacity = true;
+      capacity = hit(tag, set);
+      if(capacity){
+        capacity = miss(tag, set);
+      }
+      /*This means the tag is not in the set and is added at the back MRU */
+      if(capacity){
+        assert(capacity == true);
+        memcache.get(set).remove(0);
+        memcache.get(set).add(setassoc-1, tag);
+        /*The capacity miss incurs when there are more addresses than available */
+        capacitymiss++;
+      }
+   }
+
+   boolean hit(int tag, int set){
+        boolean capacity = true;
+        for(int x = 0; x < setassoc; x++){
+          /*Place the MRU at the back of the queue for eviction */
+          if(memcache.get(set).get(x) == tag){
+             reverseorder(x, set, tag);
+             hit++;
+             capacity = false;
+          }
+       }
+       return capacity;
+   }
+
+   boolean miss(int tag, int set){
+     boolean capacity = true;
+     for(int x = 0; x < setassoc; x++){
+         if(memcache.get(set).get(x) == null){
+      /*This checks that the address is not been put at the back of the queue, MRU policy */
+           boolean present = checkqueue(set, tag);
+           if(present){
+             capacity = false;
+           }
+           else{
+                memcache.get(set).set(x, tag);
+      /*This is a necessary miss as the cache has an empty block */
+                necessarymiss++;
+                capacity = false;
+           }
+        }
+    }
+    return capacity;
+  }
+
+   /*This checks that the tag is not placed at the back of the queue */
+   boolean checkqueue(int set, int tag){
+     for(int x = setassoc-1; x > 0; x--){
+         if(memcache.get(set).get(x) == null){
+           return false;
+         }
+         if(memcache.get(set).get(x) == tag){
+            reverseorder(x, set, tag);
+            hit++;
+            return true;
+         }
+     }
+     return false;
+   }
+
+/*This returns an int that tells where the set should be located */
+   int return_set(Long address){
+      Long result = (address >> numblock) & ((1 << numset)-1);
+      int intresult = result.intValue();
+      return intresult;
+   }
+
+/*The tag is the reference of the address and unique to the address */
+   int return_tag(Long address){
+     Long result = address >> (numset + numblock);
+     int intresult = result.intValue();
+     return intresult;
+   }
+
+/*To ensure that MRU is placed at the back of the queue */
+  void reverseorder(int position, int set, int tag){
+    memcache.get(set).remove(position);
+    memcache.get(set).add(setassoc-1, tag);
+    if(memcache.get(set).size() > setassoc){
+       System.out.println("Size of the memory cache set is greater than the blocks allocated to it");
+       System.exit(0);
+    }
+    assert(memcache.get(set).size() <= setassoc);
+  }
+
+/*To flush the cache of any previous data */
+   void flushcache(){
+       for(int i = 0; i<sumset; i++){
+        for(int x = 0; x<setassoc; x++){
+            memcache.get(i).set(x, null);
+        }
+       }
+   }
+}
